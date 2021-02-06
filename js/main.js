937fbf41cdb748769704551107e71dc182475bf3
csslick <digitone@naver.com>
2021-02-06 09:13:10 +0900

    video layout

--- js/main.js
+++ js/main.js
@@ -13,4 +13,21 @@ $(function(){
     $('#gnb').removeClass('show');
   });
 
+  $('#gnb a[href*="synop"]').on('click', function(e){
+    // e.preventDefault();
+    // $('#synop').addClass('show');
+    // location.href = './index.html?synop=1';
+    console.log('synop');
+  }) 
+  
+  console.log(location.href);
+  var val = location.href.substr(
+    location.href.lastIndexOf('=') + 1
+  );
+  if(val == 1) {
+    $('#synop').addClass('show');
+  } else {
+    $('#synop').removeClass('show');
+  }  
+
 });
