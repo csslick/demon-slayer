4ae27b7618abf96675bcb78c17c12349bfe172af
csslick <digitone@naver.com>
2021-02-07 22:22:58 +0900

    synop gnb menu active

--- js/main.js
+++ js/main.js
@@ -13,12 +13,15 @@ $(function(){
     $('#gnb').removeClass('show');
   });
 
-  // 시놉시스 팝업
-  var val = location.href.substr(
-    location.href.lastIndexOf('=') + 1
-  );
+  
+  // 시놉시스 팝업 & 메뉴 active
+  var val = location.href.substr(location.href.lastIndexOf('=') + 1);
+  console.log(val);
   if(val == 1) {
     $('#synop').addClass('show');
+    $('#gnb ul li').removeClass('active');
+    $('#gnb ul li a:contains(시놉시스)').parent().addClass('active');
+
   } else {
     $('#synop').removeClass('show');
   }  
